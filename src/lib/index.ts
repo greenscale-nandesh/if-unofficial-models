db333b86b7c6af4f8487c7f1365e56bb07afe7f8
SFulpius <36622934+SFulpius@users.noreply.github.com>
2024-03-22 11:21:57 +0100

    fix(lib): export the plugin

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,3 +5,4 @@ export {Co2js} from './co2js';
 export {TeadsAWS} from './teads-aws';
 export {TeadsCurve} from './teads-curve';
 export {WattTimeGridEmissions} from './watt-time';
+export {KeplerPlugin} from './kepler-importer';
